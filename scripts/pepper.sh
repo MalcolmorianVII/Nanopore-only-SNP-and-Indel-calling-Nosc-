51a620ee6e7af7df06231f1586f982c6ddab5f3f
MalcolmorianVII <bdkutambe@gmail.com>
2022-02-13 14:32:40 +0000

    Updated pepper to 0.7

--- scripts/pepper.sh
+++ scripts/pepper.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/bash
 species=(barcode01_Acinetobacter_baumannii_J9 barcode02_Citrobacter_koseri_MINF_9D barcode03_Enterobacter_kobei_MSB1_1B barcode04_Haemophilus_unknown_M1C132_1 barcode05_Klebsiella_oxytoca_MSB1_2C barcode06_CHF10J1 barcode07_Klebsiella_variicola_INF345 barcode08_Serratia_marcescens_17-147-1671)
 ref=/home/ubuntu/data/belson/bioinformatics/projects_2021/nosc/reference_genomes
-BASE="/home/ubuntu/data/belson/bioinformatics/projects_2021/nosc/results/nosc_pepper/2022.01.02"
-mapped=/home/ubuntu/data/belson/bioinformatics/projects_2021/nosc/results/nosc_pepper/2022.01.02
+BASE="/home/ubuntu/data/belson/bioinformatics/projects_2021/nosc/results/nosc_pepper/2022.02.07"
+mapped=/home/ubuntu/data/belson/bioinformatics/projects_2021/nosc/results/nosc_pepper/2022.02.07
 # Set the number of CPUs to use
 THREADS="8"
 
@@ -39,14 +39,14 @@ pepper(){
 	docker run --ipc=host \
 	-v $1:$1\
 	-v $2:$2\
-	kishwars/pepper_deepvariant:r0.5 \
+	kishwars/pepper_deepvariant:r0.7 \
 	run_pepper_margin_deepvariant call_variant \
 	-b ${1}/${3} \
 	-f ${2}/${4} \
 	-o ${1} \
 	-p "$5" \
 	-t $6 \
-	--ont
+	--ont_r9_guppy5_sup
 }
 
 minimap 
@@ -56,12 +56,23 @@ do
 	# mkdir -p ${dir}/pepper_results
 	slashed=$(basename $dir | sed 's/_pepper_results//')
 	BAM=${slashed}.bam
-	REF=${slashed}_ref.fna
-	if [ -d ${dir}/{slashed}.vcf.gz ]
+	ref_genome=${slashed}_ref.fna
+	if [[ -f ${dir}/${slashed}.vcf.gz && `stat -c %s ${dir}/${slashed}.vcf.gz` -ne 0 ]]
 	then
-		echo "PREVIOUS PEPPER RUN WAS SUCCESSFUL"
+		echo "PREVIOUS PEPPER RUN SUCCESSFUL"
+		echo "EXITING....."
 	else
-		pepper ${dir} ${ref} ${BAM} ${REF} ${slashed} ${THREADS}
+		pepper ${dir} ${ref} ${BAM} ${ref_genome} ${slashed} ${THREADS}
+	fi
+	if [[ -f ${dir}/${slashed}_pepper.norm.vcf.gz && `stat -c %s ${dir}/${slashed}_pepper.norm.vcf.gz` -ne 0 ]]
+	then
+		echo "No need for normalization"
+	else
+		eval "$(conda shell.bash hook)"
+		conda activate "bcf"
+		normVCF=${mapped}/${slashed}_pepper_results/${slashed}_pepper.norm.vcf.gz
+		bcftools norm  -f ${ref}/${ref_genome} ${dir}/${slashed}.vcf.gz  -Oz -o $normVCF
+		tabix -p vcf $normVCF
 	fi
 done
 
